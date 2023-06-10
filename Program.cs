ccb1a1cb5fe8090fdd524da31d9291b0cbd37d59
Kelsei Kyser <kkelsei127@yahoo.com>
2023-06-10 17:46:10 -0400

    added functionality for async await creation of files

--- Program.cs
+++ Program.cs
@@ -21,8 +21,44 @@ namespace ReadMeGeneratorCS
                 // exit from app if no name entered
 
                 Console.WriteLine("What license did the application use?");
+                string license = Console.ReadLine() ?? "";
 
+                Console.WriteLine("What is the purpose of this application? Give a detailed description.");
+                string description = Console.ReadLine() ?? "";
+
+                Console.WriteLine("How do you install this application? List any additional tools needed.");
+                string installation = Console.ReadLine() ?? "";
+
+                Console.WriteLine("What are the intended uses for this application?");
+                string usages = Console.ReadLine() ?? "";
+
+                Console.WriteLine("Who are the contributors to this application?");
+                string contributors = Console.ReadLine() ?? "";
+                
+                Console.WriteLine("How do you test this application?");
+                string testing = Console.ReadLine() ?? "";
+
+                Console.WriteLine("What is your Github username?");
+                string github = Console.ReadLine() ?? "";
+
+                Console.WriteLine("Do you have any other contact info to add?");
+                string contactInfo = Console.ReadLine() ?? "";
+
+                Prompt currentPrompt = new Prompt(applicationName, license, description, installation, usages, contributors, testing, github, contactInfo);
+                prompts.Add(currentPrompt);
             }
+            return prompts;
+        }
+
+
+        async static Task Main(string[] args)
+
+        {
+            List<Prompt> prompts = GetPrompts();
+            Util.PrintReadMe(prompts);
+            Util.MakeCSV(prompts);
+            //not sure how to make a md file, currently will have it made into a csv?
+            await Util.MakeReadMe(prompts);
         }
     }
 }
